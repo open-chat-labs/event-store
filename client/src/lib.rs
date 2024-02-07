ae4ef6423bce32df3dde5495a86f687f58f5e455
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-07 17:20:32 +0000

    Use builder pattern to build Client instances (#6)

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,13 +3,15 @@ use std::mem;
 use std::sync::{Arc, Mutex, MutexGuard};
 use std::time::Duration;
 
+const DEFAULT_FLUSH_DELAY: Duration = Duration::from_secs(300);
+
 pub struct Client<R> {
     inner: Arc<Mutex<ClientInner<R>>>,
 }
 
 struct ClientInner<R> {
     runtime: R,
-    flush_interval: Duration,
+    flush_delay: Duration,
     next_flush_scheduled: Option<TimestampMillis>,
     events: Vec<IdempotentEvent>,
 }
@@ -28,28 +30,49 @@ pub trait Runtime {
 }
 
 impl<R> Client<R> {
-    pub fn new(runtime: R, flush_interval: Duration) -> Client<R> {
-        Client {
-            inner: Arc::new(Mutex::new(ClientInner::new(runtime, flush_interval))),
-        }
-    }
-
     pub fn take_events(&mut self) -> Vec<IdempotentEvent> {
         mem::take(&mut self.inner.lock().unwrap().events)
     }
 }
 
-impl<R: Runtime + Send + 'static> Client<R> {
-    pub fn init_with_events(
-        runtime: R,
-        flush_interval: Duration,
-        events: Vec<IdempotentEvent>,
-    ) -> Client<R> {
-        let client = Client::new(runtime, flush_interval);
-        client.requeue_events(events);
-        client
+pub struct ClientBuilder<R> {
+    runtime: R,
+    flush_delay: Option<Duration>,
+    events: Vec<IdempotentEvent>,
+}
+
+impl<R: Runtime + Send + 'static> ClientBuilder<R> {
+    pub fn new(runtime: R) -> ClientBuilder<R> {
+        ClientBuilder {
+            runtime,
+            flush_delay: None,
+            events: Vec::new(),
+        }
     }
 
+    pub fn with_flush_delay(mut self, duration: Duration) -> Self {
+        self.flush_delay = Some(duration);
+        self
+    }
+
+    pub fn with_events(mut self, events: Vec<IdempotentEvent>) -> Self {
+        self.events = events;
+        self
+    }
+
+    pub fn build(self) -> Client<R> {
+        let flush_delay = self.flush_delay.unwrap_or(DEFAULT_FLUSH_DELAY);
+        let client = Client {
+            inner: Arc::new(Mutex::new(ClientInner::new(self.runtime, flush_delay))),
+        };
+        if !self.events.is_empty() {
+            client.requeue_events(self.events);
+        }
+        client
+    }
+}
+
+impl<R: Runtime + Send + 'static> Client<R> {
     pub fn push_event(&mut self, event: Event) {
         let mut guard = self.inner.lock().unwrap();
         let idempotency_key = guard.runtime.rng();
@@ -72,16 +95,17 @@ impl<R: Runtime + Send + 'static> Client<R> {
         if guard.next_flush_scheduled.is_none() {
             let clone = self.clone();
             let now = guard.runtime.now();
-            let flush_interval = guard.flush_interval;
+            let flush_delay = guard.flush_delay;
             guard
                 .runtime
-                .schedule_flush(flush_interval, move || clone.flush_events());
-            guard.next_flush_scheduled = Some(now + flush_interval.as_millis() as u64);
+                .schedule_flush(flush_delay, move || clone.flush_events());
+            guard.next_flush_scheduled = Some(now + flush_delay.as_millis() as u64);
         }
     }
 
     fn flush_events(&self) {
         let mut guard = self.inner.lock().unwrap();
+        guard.next_flush_scheduled = None;
         let events = mem::take(&mut guard.events);
 
         if !events.is_empty() {
@@ -102,10 +126,10 @@ impl<R> Clone for Client<R> {
 }
 
 impl<R> ClientInner<R> {
-    pub fn new(runtime: R, flush_interval: Duration) -> ClientInner<R> {
+    pub fn new(runtime: R, flush_delay: Duration) -> ClientInner<R> {
         ClientInner {
             runtime,
-            flush_interval,
+            flush_delay,
             next_flush_scheduled: None,
             events: Vec::new(),
         }
