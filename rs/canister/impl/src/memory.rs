fd50d5a81e7db863ca5aeb03d04d68edc3b78d9b
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-26 10:45:18 +0000

    Increase stable memory bucket size to 128 wasm pages

--- rs/canister/impl/src/memory.rs
+++ rs/canister/impl/src/memory.rs
@@ -1,15 +1,27 @@
+use ic_cdk::api::stable::StableWriter;
 use ic_stable_structures::{
     memory_manager::{MemoryId, MemoryManager, VirtualMemory},
     DefaultMemoryImpl,
 };
+use std::cell::RefCell;
 
 const UPGRADES: MemoryId = MemoryId::new(0);
 
 pub type Memory = VirtualMemory<DefaultMemoryImpl>;
 
 thread_local! {
-    static MEMORY_MANAGER: MemoryManager<DefaultMemoryImpl>
-        = MemoryManager::init_with_bucket_size(DefaultMemoryImpl::default(), 16);
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>>
+        = RefCell::new(MemoryManager::init_with_bucket_size(DefaultMemoryImpl::default(), 128));
+}
+
+// This forces the buckets to be the specified size rather than preserving the previous bucket size
+pub fn reset_memory_manager() {
+    let mut writer = StableWriter::default();
+    writer.write(&[0, 0, 0]).unwrap();
+    MEMORY_MANAGER.replace(MemoryManager::init_with_bucket_size(
+        DefaultMemoryImpl::default(),
+        128,
+    ));
 }
 
 pub fn get_upgrades_memory() -> Memory {
@@ -17,5 +29,5 @@ pub fn get_upgrades_memory() -> Memory {
 }
 
 fn get_memory(id: MemoryId) -> Memory {
-    MEMORY_MANAGER.with(|m| m.get(id))
+    MEMORY_MANAGER.with_borrow(|m| m.get(id))
 }
