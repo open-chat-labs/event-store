8d5c71434a699aef28ab513d3fa23f8da0999ccf
Hamish Peebles <hamishpeebles@gmail.com>
2024-05-03 10:45:56 +0100

    Switch canister type to "rust" and use DFX to build it (#77)

--- scripts/docker-build-wasm.sh
+++ scripts/docker-build-wasm.sh
@@ -6,13 +6,25 @@ cd $SCRIPT_DIR/..
 
 GIT_COMMIT_ID=$(git rev-parse HEAD)
 
+if [ -z "${CARGO_HOME}" ]
+then
+  CARGO_HOME="${HOME}/.cargo"
+fi
+
+RUSTFLAGS="--remap-path-prefix $(readlink -f ${SCRIPT_DIR}/..)=/build --remap-path-prefix ${CARGO_HOME}/bin=/cargo/bin --remap-path-prefix ${CARGO_HOME}/git=/cargo/git"
+for l in $(ls ${CARGO_HOME}/registry/src/)
+do
+  RUSTFLAGS="--remap-path-prefix ${CARGO_HOME}/registry/src/${l}=/cargo/registry/src/github ${RUSTFLAGS}"
+done
+
 echo "CommitId: $GIT_COMMIT_ID"
 
-docker build -t event_store --build-arg git_commit_id=$GIT_COMMIT_ID .
+docker build -t event_store --build-arg git_commit_id=$GIT_COMMIT_ID --build-arg rustflags="$RUSTFLAGS" .
 
 container_id=$(docker create event_store)
 rm -rf wasms
-docker cp $container_id:/build/wasms wasms
+mkdir wasms
+docker cp $container_id:/build/.dfx/local/canisters/event_store/event_store.wasm.gz wasms
 docker rm --volumes $container_id
 
 cd wasms
