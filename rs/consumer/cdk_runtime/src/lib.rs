c1b194358e8e67d48d9b6a01272dab14d6bda925
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-17 16:52:20 +0000

    Fix

--- rs/consumer/cdk_runtime/src/lib.rs
+++ rs/consumer/cdk_runtime/src/lib.rs
@@ -1,9 +1,9 @@
 use event_store_canister::{EventsArgs, EventsResponse};
 use event_store_consumer::Runtime;
+use ic_cdk::call::{Call, CallFailed};
 use ic_principal::Principal;
 use serde::{Deserialize, Serialize};
 use std::future::Future;
-use ic_cdk::call::{Call, CallFailed};
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct CdkRuntime;
@@ -14,12 +14,17 @@ impl CdkRuntime {
         canister_id: Principal,
         args: EventsArgs,
     ) -> Result<EventsResponse, (i32, String)> {
-        match Call::unbounded_wait(canister_id, "events").with_arg(args).await {
-            Ok(response) => {
-                response.candid().map_err(|e| (0, format!("Failed to deserialize response: {e}")))
-            },
+        match Call::unbounded_wait(canister_id, "events")
+            .with_arg(args)
+            .await
+        {
+            Ok(response) => response
+                .candid()
+                .map_err(|e| (0, format!("Failed to deserialize response: {e}"))),
             Err(CallFailed::CallPerformFailed(f)) => Err((0, f.to_string())),
-            Err(CallFailed::CallRejected(r)) => Err((u32::from(r.reject_code()) as i32, r.reject_message().to_string())),
+            Err(CallFailed::CallRejected(r)) => {
+                Err((r.raw_reject_code() as i32, r.reject_message().to_string()))
+            }
         }
     }
 }
